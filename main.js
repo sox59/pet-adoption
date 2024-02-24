e93066708e46f023b28684066f75ad08e27fa1e1
Paul <ppagliughi@yahoo.com>
2024-02-24 15:51:51 -0600

    Added pet name

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
+
+
+
+
+
 async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/LOT/73,78/forecast")
     const weatherData = await weatherPromise.json()
@@ -14,13 +22,16 @@ async function petsArea() {
 
     const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
     const petsData = await petsPromise.json()
-    // console.log(petsPromise)
-    // console.log(petsData)
     petsData.forEach(pet => {
 
-        console.log(pet.name)
+        const clone = template.content.cloneNode(true)
+
+        clone.querySelector("h3").textContent = pet.name
+        wrapper.appendChild(clone)
 
     })
+
+    document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 
 petsArea()
