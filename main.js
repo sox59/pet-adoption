2dc831c915416f419a8cb1569312cd90f5be1d6a
Paul <ppagliughi@yahoo.com>
2024-03-01 19:31:49 -0600

    Finished pet filtering

--- main.js
+++ main.js
@@ -26,6 +26,8 @@ async function petsArea() {
 
         const clone = template.content.cloneNode(true)
 
+        clone.querySelector(".pet-card").dataset.species = pet.species
+
         clone.querySelector("h3").textContent = pet.name
         clone.querySelector(".pet-description").textContent = pet.description
         clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
@@ -65,7 +67,7 @@ function handleButtonClick(e) {
 
     // remove active class from any and all buttons
 
-    allButtons.forEach(el => el.classList.remove("active"))
+    allButtons.forEach(x => x.classList.remove("active"))
 
 
     // add active class to the specific button that just got clicked
@@ -73,4 +75,27 @@ function handleButtonClick(e) {
 
     // filter the pets down below
 
+    const currentFilter = e.target.dataset.filter
+    document.querySelectorAll(".pet-card").forEach(el => {
+
+        if (currentFilter == el.dataset.species || currentFilter == "all") {
+
+            el.style.display = "grid"
+
+
+
+        } else {
+
+
+            el.style.display = "none"
+
+
+
+        }
+
+    })
+
+
+
 }
+
